502a202a7bb401d9cca8acb52a25ac0ca906dde4
Ferdinand Jason G <ferdinandjasong@gmail.com>
2017-10-24 21:57:50 +0700

    Commit soal3.c main()

--- soal3.c
+++ soal3.c
@@ -9,14 +9,14 @@ int hp_kepiting=100;
 
 void *starving_lohan(void *argv){
     while(1){
-        sleep(1);
+        sleep(10);
         hp_lohan-=15;
     }
 }
 
 void *starving_kepiting(void *argv){
     while(1){
-        sleep(2);
+        sleep(20);
         hp_kepiting-=10;
     }
 }
@@ -31,5 +31,21 @@ void *add_hp_kepiting(void *argv){
 
 
 int main(){
-    
+    pthread_t tid[4];
+    pthread_create(&(tid[2]),NULL,starving_lohan,NULL);
+    pthread_create(&(tid[3]),NULL,starving_kepiting,NULL);
+    while(1){
+        int n;scanf("%d",&n);
+        if(n==1){
+            pthread_create(&(tid[0]),NULL,add_hp_lohan,NULL);
+        }
+        else if(n==2){
+            pthread_create(&(tid[1]),NULL,add_hp_kepiting,NULL);
+        }
+        printf("%d %d\n",hp_lohan,hp_kepiting);
+        if(hp_lohan>100 || hp_lohan<=0 || hp_kepiting>100 || hp_kepiting<=10){
+            printf("selesai ~~");
+            break;
+        }
+    }
 }
