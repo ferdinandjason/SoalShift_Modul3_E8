89b297eef6ab19f80a768d6c98669df455345831
nurlitadf <nurlitadf17@gmail.com>
2017-10-24 21:18:40 +0700

    Commit soal4.c with countc()

--- soal4.c
+++ soal4.c
@@ -19,6 +19,12 @@ void* countb(void *argv){
     printf("Hasil %lld! = %lld\n",b,num);
     return NULL;
 }
+void* countb(void *argv){
+    ll num=1;
+    for(ll i=2;i<=c;i++) num*=i;
+    printf("Hasil %lld! = %lld\n",c,num);
+    return NULL;
+}
 int main(){
     scanf("%lld %lld %lld",&a,&b,&c);
     pthread_create(&(tid[0]),NULL,&count1,NULL);
